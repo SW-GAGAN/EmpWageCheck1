f051409b027a93dafa58c95a0eb7c0106e4668bf
SW-GAGAN <srgagan73@gmail.com>
2021-07-12 19:20:57 +0530

    Adding: random to check emp present

--- com/bridgelab/emp/EmpWage.java
+++ com/bridgelab/emp/EmpWage.java
@@ -2,6 +2,11 @@ package com.bridgelab.emp;
 
 public class EmpWage {
 	public static void main(String[] args){
-		System.out.println("welcome to employe wage");
+		int IS_Full_Time = 1;
+		double empCheck = Math.floor(Math.random() * 100) % 2 ;
+		if ( empCheck == IS_Full_Time )
+			System.out.println(" Employee is present");
+		else
+			System.out.println("employee is absent");
 	}
 }
